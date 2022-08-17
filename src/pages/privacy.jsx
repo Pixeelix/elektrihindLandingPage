292f81dacb85a2914893cb46053a699cdb48200e
Martin Pihooja <martin.pihooja@gmail.com>
2022-08-18 00:05:59 +0300

    fix ' bug

--- src/pages/privacy.jsx
+++ src/pages/privacy.jsx
@@ -19,12 +19,12 @@ export default function Privacy() {
         <Container className="absolute top-1/2 left-20 -translate-y-1/2 sm:left-1/2 sm:-translate-x-1/2">
           <div className="mx-auto max-w-md sm:text-center">
             <h2 className="text-3xl font-medium tracking-tight text-white sm:text-4xl">
-              We don't store your data, period.
+              We don´t store your data, period.
             </h2>
             <p className="mt-4 text-lg text-gray-300">
-              We physically can't. We have nowhere to store it. We don't even
+              We physically can´t. We have nowhere to store it. We don´t even
               have a server database to store it. So even if Justin Bieber asked
-              nicely to see your data, we wouldn't have anything to show him.
+              nicely to see your data, we wouldn´t have anything to show him.
             </p>
           </div>
         </Container>
