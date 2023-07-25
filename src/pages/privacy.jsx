167f74d39e77fb8e88430b7d8865e451b5a6b895
Martin Pihooja <martin.pihooja@gmail.com>
2023-07-25 23:35:08 +0300

    Privacy policy update

--- src/pages/privacy.jsx
+++ src/pages/privacy.jsx
@@ -9,22 +9,37 @@ export default function Privacy() {
         <title>Privacy</title>
       </Head>
 
+
       <section
-        id="get-free-shares-today"
-        className="h-full overflow-hidden bg-gray-900 py-20 sm:py-28"
+        id="privacy"
+        aria-label="Features for investing all your money"
+        className="bg-gray-900 py-20 sm:py-32"
       >
-        <div className="absolute top-1/2 left-20 -translate-y-1/2 sm:left-1/2 sm:-translate-x-1/2">
-          <CircleBackground color="#fff" className="animate-spin-slower" />
-        </div>
-        <Container className="absolute top-1/2 left-20 -translate-y-1/2 sm:left-1/2 sm:-translate-x-1/2">
-          <div className="mx-auto max-w-md sm:text-center">
-            <h2 className="text-3xl font-medium tracking-tight text-white sm:text-4xl">
-              We don´t store your data, period.
+        <Container>
+          <div className="mx-auto max-w-fit max-h-screen lg:mx-0 lg:max-w-fit">
+            <h2 className="text-3xl font-medium tracking-tight text-white">
+              Privacy policy
             </h2>
-            <p className="mt-4 text-lg text-gray-300">
-              We physically can´t. We have nowhere to store it. We don´t even
-              have a server database to store it. So even if Justin Bieber asked
-              nicely to see your data, we wouldn´t have anything to show him.
+            <p className="mt-2 text-lg text-gray-400">
+              To improve the performance of AdMob, the Google Mobile Ads SDK may collect certain information from apps, including:
+              <ul class="list-disc">
+                <li>IP address, which may be used to estimate the general location of a device.</li>
+              </ul>
+              <ul class="list-disc">
+                <li>Non-user related crash logs, which may be used to diagnose problems and improve the SDK. Diagnostic information may also be used for advertising and analytics purposes.</li>
+              </ul>
+              <ul class="list-disc">
+                <li>User-associated performance data such as app launch time, hang rate, or energy usage, which may be used to evaluate user behavior, understand the effectiveness of existing product features, and plan new features. Performance data may also be used for displaying ads, including sharing with other entities that display ads.</li>
+              </ul>
+              <ul class="list-disc">
+                <li>A Device ID, such as the device's advertising identifier or other app- or developer-bounded device identifiers, which may be used for the purpose of third-party advertising and analytics.</li>
+              </ul>
+              <ul class="list-disc">
+                <li>Advertising data, such as advertisements the user has seen, may be used to power analytics and advertising features.</li>
+              </ul>
+              <ul class="list-disc">
+                <li>Other user product interactions like app launch taps, and interaction information, like video views, may be used to improve advertising performance.</li>
+              </ul>
             </p>
           </div>
         </Container>
